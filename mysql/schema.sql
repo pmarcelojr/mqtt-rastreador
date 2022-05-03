a53dcddf2c2ba53a019a588c775d27d1f34de047
Marcelo Santos <marcelosantostecnologia@gmail.com>
2022-05-03 00:26:08 -0300

    Melhorias em SQL e docker-compose

--- mysql/schema.sql
+++ mysql/schema.sql
@@ -1,3 +1,11 @@
+CREATE TABLE `user` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `nome` varchar(40) NOT NULL,
+  `username` varchar(20)  UNIQUE NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `email` varchar(320) UNIQUE DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `acls` (
   `id` bigint NOT NULL AUTO_INCREMENT,
@@ -8,7 +16,6 @@ CREATE TABLE `acls` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-
 CREATE TABLE `dispositivo` (
   `id` varchar(255) NOT NULL,
   `admin` int DEFAULT NULL,
@@ -17,12 +24,9 @@ CREATE TABLE `dispositivo` (
   `senha` varchar(255) NOT NULL,
   `topico` varchar(255) NOT NULL,
   `ultima_atualizacao` datetime DEFAULT NULL,
-  `ultima_localizacao_id` varchar(255) DEFAULT NULL,
   `user_id` bigint NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `UKfnm57bnjfq1mey2agt91ndsat` (`nome`,`user_id`),
-  KEY `FKf577tpblghjwchowt2rjlj06v` (`ultima_localizacao_id`),
-  KEY `FKe1b91y83rjtflsvna0o5u20bo` (`user_id`),
-  CONSTRAINT `FKe1b91y83rjtflsvna0o5u20bo` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
-  CONSTRAINT `FKf577tpblghjwchowt2rjlj06v` FOREIGN KEY (`ultima_localizacao_id`) REFERENCES `localizacao` (`id`)
+  UNIQUE KEY `dispositivo_un` (`nome`,`user_id`),
+  KEY `dispositivo_user_fk` (`user_id`),
+  CONSTRAINT `dispositivo_user_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
